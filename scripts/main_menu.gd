e52bac5f39e32a373d8e56325fc8ced74e0108ae
GrandmaLucas <105500227+GrandmaLucas@users.noreply.github.com>
2024-12-01 09:40:58 -0400

    Add label and button sound to intro cutscene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 signal start_game
+@onready var buttonclick: AudioStreamPlayer = $"../camera_rig/buttonclick"
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -9,10 +10,12 @@ func _ready() -> void:
 	
 func _on_play_pressed() -> void:
 	self.visible = false
+	buttonclick.play()
 	emit_signal("start_game")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _on_quit_pressed() -> void:
+	buttonclick.play()
 	get_tree().quit()
 
